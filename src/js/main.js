96074d099c39668f6667a02938b84d07e2c4cab7
elena.alcaraz.martin@gmail.com <elena.alcaraz.martin@gmail.com>
2024-04-26 19:11:15 +0200

    Buscador y reset

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,21 @@
 
 //variables globales
 const ulList = document.querySelector('.js_coctails');
+const inputSearch = document.querySelector('.js_search');
+const buttonSearch = document.querySelector('.js-btn-search');
+const SERVER_URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`;
+const buttonReset = document.querySelector('.js_reset');
 
 let coctailsData = []
 
+
+//RENDERIZAR LAS BEBIDAS
 const renderOneCoctail = (eachCoctail) => {
-    return `<li> 
+    return `<li class= "card"> 
         <h6>${eachCoctail.strDrink}</h6>
         <img src="${eachCoctail.strDrinkThumb}"/>
     </li>`
+    //¿meter aquí condicional para las bebidas que no tengan foto? 
 };
 
 const renderAllCoctails = () => {
@@ -18,8 +25,10 @@ const renderAllCoctails = () => {
     }
 };
 
+
+// FETCH A LA API
 const getData = () => {
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+    fetch(SERVER_URL + inputSearch.value)
         .then(response => response.json())
         .then(dataApi => {
             coctailsData = dataApi.drinks;
@@ -28,7 +37,23 @@ const getData = () => {
         })
 };
 
+//BUSCAR
+function search () {
+    console.log(inputSearch.value);
+    ulList.innerHTML = '';
+    getData();
+};
+
+buttonSearch.addEventListener('click', search);
+
+//RESETEAR
+function reset () {
+    inputSearch.value = '';
+    ulList.innerHTML = '';
+};
+
+buttonReset.addEventListener('click', reset);
 
 
-//se ejecuta cuando carga la página
-getData(); 
+//getData(); >> esto me hace que la página empiece siendo en blanco, preguntar a Dayana
+//

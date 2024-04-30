7eab55575c4ccf6f6659e03aa4a5ef5b5a8e7450
elena.alcaraz.martin@gmail.com <elena.alcaraz.martin@gmail.com>
2024-04-30 12:18:38 +0200

    Final

--- src/js/main.js
+++ src/js/main.js
@@ -8,12 +8,12 @@ const buttonSearch = document.querySelector('.js-btn-search');
 const SERVER_URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`;
 const buttonReset = document.querySelector('.js_reset');
 const favList = document.querySelector('.js_coctails_favorite');
-
+const resetFav = document.querySelector('.js_reset_favorites');
 
 let coctailsData = []
 let coctailsFavorite = [];
 
-//BUSCAR
+//Función para buscar
 function search () {
     ulList.innerHTML = '';
     if (inputSearch.value === '') {
@@ -23,17 +23,29 @@ function search () {
         getData(inputSearch.value);
     }
 };
-
 buttonSearch.addEventListener('click', search);
 
-//RESETEAR
+//Función para resetear la lista genérica
 function reset () {
     inputSearch.value = '';
     ulList.innerHTML = '';
 };
-
 buttonReset.addEventListener('click', reset);
 
+//Función para resetear la lista de favs
+function resetFavorites () {
+    favList.innerHTML = '';
+    coctailsFavorite = [];
+    localStorage.removeItem('coctailsFavorite');
+    resetFav.classList.add('hidden');
+    //eliminamos la clase blue cuando reseteamos favoritos
+    const blueElements = document.querySelectorAll('.blue');
+    // Iterar sobre los elementos y eliminar la clase "blue"
+    blueElements.forEach(element => {
+        element.classList.remove('blue');
+    });
+};
+resetFav.addEventListener('click', resetFavorites);
 
 // FETCH A LA API
 const getData = (value) => {
@@ -42,7 +54,7 @@ const getData = (value) => {
         .then(dataApi => {
             coctailsData = dataApi.drinks;
             if (coctailsData === null) {
-                alert('No se han encontrado resultados')
+                alert('No se han encontrado resultados ):')
             } else {
                 renderAllCoctails();
             }
@@ -50,16 +62,12 @@ const getData = (value) => {
         })
 };
 
-getData("margarita");
-
 //RENDERIZAR UNA BEBIDA
 const renderOneCoctail = (eachCoctail) => {
     let imgCoctail = eachCoctail.strDrinkThumb;
     if (imgCoctail === null) {
         imgCoctail = 'https://media.glamour.mx/photos/632371dd8645b8d42bd2a706/master/pass/cocteles-para-el-15-de-septiembre.jpg'
     }
-    //convertir en operador ternario lo de arriba?
-
     return `<li class="card js_coctails_li" id="${eachCoctail.idDrink}"> 
         <h6>${eachCoctail.strDrink}</h6>
         <img src="${imgCoctail}"/>
@@ -69,26 +77,24 @@ const renderOneCoctail = (eachCoctail) => {
 //RENDERIZAR TODAS LAS BEBIDAS
 const renderAllCoctails = () => {
     //pintamos los cócteles
+    ulList.innerHTML = "" ; 
     for (let i = 0; i < coctailsData.length; i++) {
         ulList.innerHTML += renderOneCoctail(coctailsData[i])
     }
-
     //asignamos el evento click a cada uno de los cócteles
     const allCoctailsLi = document.querySelectorAll('.js_coctails_li');
     for (let i = 0; i < allCoctailsLi.length; i++) {
         allCoctailsLi[i].addEventListener('click', addFavorite);
-
     }
-
 };
 
-///añadir favorito
+//Función añadir favorito
 const addFavorite = (ev) => {
     const idCoctail = ev.currentTarget.id;
     //obtenemos todos los datos del coctail clicado. 
     const clickedCoctail = coctailsData.find(item => item.idDrink === idCoctail);
 
-    //verificar si la paleta cliada ya es un fav
+    //verificar si el coctail cliado ya es un fav
     const isFavoriteCoctailIndex = coctailsFavorite.findIndex(item => item.idDrink === idCoctail);
     //condicional para añadir al array si no está, y quitarla si clicamos de nuevo
     if (isFavoriteCoctailIndex === -1) {
@@ -98,12 +104,17 @@ const addFavorite = (ev) => {
     } else {
         //si está, lo quitamos de favoritos
         coctailsFavorite.splice(isFavoriteCoctailIndex, 1);
-        ev.currentTarget.classList.remove('blue');
+        ev.currentTarget.classList.remove('blue')
     };
+
+    //añadimos el botón para poder limpiar todos los favoritos
+    resetFav.classList.remove('hidden');
     
-    //añadir la clase a los que se clican para favoritos
-    //ev.currentTarget.classList.toggle('blue');
     renderFavorites();
+
+    if (coctailsFavorite.length === 0) {
+        resetFav.classList.add('hidden');
+    }
 
     //guardar favoritos en el local storage
     localStorage.setItem('coctailsFavorite', JSON.stringify(coctailsFavorite)); 
@@ -111,20 +122,20 @@ const addFavorite = (ev) => {
 
 //Renderizar un favorito
 const renderOneFavoriteCoctail = (eachCoctail) => {
-    return `<li class="card fav_coctail js_coctails_li">
+    return `<li class="card fav_coctail">
         <button class="btn_remove js_remove_fav" id="${eachCoctail.idDrink}">Eliminar de favoritos</button>
         <h6>${eachCoctail.strDrink}</h6>
         <img src="${eachCoctail.strDrinkThumb}"/>
     </li>`
 };
 
-//renderizar todos los favoritos + evento sobre el botón eliminar
+//Renderizar todos los favoritos + evento sobre el botón eliminar
 function renderFavorites () {
     favList.innerHTML = '';
-    for (let i = 0; i < coctailsFavorite.length; i++) {
-        favList.innerHTML += renderOneFavoriteCoctail (coctailsFavorite[i])
+    for (const fav of coctailsFavorite) {
+        favList.innerHTML += renderOneFavoriteCoctail (fav)
     }
-
+    //asignamos el evento click a cada uno de los botones
     const allBtnFav = document.querySelectorAll('.js_remove_fav');
     for (let i = 0; i < allBtnFav.length; i++) {
         allBtnFav[i].addEventListener('click', removeFavorite);
@@ -136,7 +147,7 @@ const removeFavorite = (ev) => {
     //obtenemos todos los datos del botón de eliminar clicado. 
     const clickedCoctail = coctailsData.find(item => item.idDrink === idCoctail);
 
-    //verificar si el coctail cliada ya es un fav
+    //verificar si el coctail cliado ya es un fav
     const isFavoriteCoctailIndex = coctailsFavorite.findIndex(item => item.idDrink === idCoctail);
     //condicional para añadir al array si no está, y quitarla si clicamos de nuevo
     if (isFavoriteCoctailIndex === -1) {
@@ -147,12 +158,20 @@ const removeFavorite = (ev) => {
         coctailsFavorite.splice(isFavoriteCoctailIndex, 1);
     };
 
-    
+
     renderFavorites();
     
+    //llamamos de nuevo a la función renderAllCoctails para que pase por los favoritos
+    renderAllCoctails();
+
+    if (coctailsFavorite.length === 0) {
+        resetFav.classList.add('hidden');
+    }
+
     //sobreescribimos el local storage 
     localStorage.setItem('coctailsFavorite', JSON.stringify(coctailsFavorite)); 
 };
+
 
 //renderizamos lo que tenemos en el localStorage
 function renderLocalStorage () {
@@ -163,4 +182,16 @@ function renderLocalStorage () {
     }
 };
 
+
+function btnResetFavorites () {
+    if (coctailsFavorite.length === 0) {
+        resetFav.classList.add('hidden');
+    } else {
+        resetFav.classList.remove('hidden');
+    }
+}; 
+
+//Al cargar la página
 renderLocalStorage();
+btnResetFavorites();
+getData("margarita");
